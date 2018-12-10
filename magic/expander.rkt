976030ff7d4af5b6d25dc62aca6dc35371d5299f
jjsimpso <jjsimpso@a8c7db1f-5b64-4c47-b761-cd94ac1c0e2a>
2018-12-10 18:04:18 +0000

    magic: clean up expander a little. change phase of functions needed during expansion.

--- magic/expander.rkt
+++ magic/expander.rkt
@@ -9,7 +9,7 @@
        (printf "q is ~a~n" q)
        (datum->syntax stx q))]))
 
-
+;; probably don't need these
 (define (make-expr test)
   (list test))
 
@@ -23,16 +23,17 @@
   (cdr test))
 
 ;; not final. doesn't work if level drops. need a new algorithm.
+;; probably not going to use
 (define (build-exp lines last-line cur-level new-level)
   (cond 
-    [(empty? lines) (make-expr (make-test (car last-line) empty))]
+    [(null? lines) (make-expr (make-test (car last-line) '()))]
     [(and (pair? (car lines)) 
           (equal? (caar lines) 'level))
      (printf "increment level~n") 
      (build-exp (cdr lines) last-line cur-level (add1 new-level))]
     [(= new-level (add1 cur-level))
      (printf "add new line with higher level~n") 
-     (if (empty? last-line)
+     (if (null? last-line)
          (build-exp (cdr lines) 
                     (car lines) 
                     (add1 cur-level) 
@@ -45,18 +46,18 @@
                                            0)))))]
     [(= new-level cur-level)
      (printf "new line at same level~n") 
-     (if (empty? last-line)
+     (if (null? last-line)
          (build-exp (cdr lines) 
                     (car lines)
                     cur-level 
                     0)
-         (cons (make-test (car last-line) empty) 
+         (cons (make-test (car last-line) '()) 
                (build-exp (cdr lines) (car lines) cur-level 0)))]
     ;[(< new-level cur-level)
      
     ))
 
-(define (eat-lines-with-greater-level lines level)
+(define-for-syntax (eat-lines-with-greater-level lines level)
   (define next-line-level
     (for/fold ([level 0])
               ([line lines])
@@ -74,8 +75,9 @@
      (eat-lines-with-greater-level (cdr next-lines) level)]
     [else
      lines]))
-  
-(define (parse-levels lines level)
+
+;; ex: (parse-levels '((line1) (level) (line2) (level) (level) (line3) (level) (line4) (level) (level) (line5)) 0)  
+(define-for-syntax (parse-levels lines level)
   ;; count level of next line
   (define next-line-level
     (for/fold ([level 0])
@@ -88,7 +90,7 @@
   (define next-lines (list-tail lines next-line-level))
   
   (cond
-    [(empty? lines) empty]
+    [(null? lines) '()]
     [(= next-line-level level)
      (cons (car next-lines)
            (parse-levels (cdr next-lines) level))]
@@ -97,12 +99,21 @@
                  (cons (car next-lines) 
                        (parse-levels (cdr next-lines) (add1 level))))
            (parse-levels (eat-lines-with-greater-level lines level) level))]
-    [(< next-line-level level) empty]))
+    [(< next-line-level level) '()]))
+
+;; (define-syntax level
+;;   (syntax-rules ()
+;;     [(level line ...)
+;;      (cond [line #t]
+;;            ...)]
+;;     [_ expr]))
 
 (define-syntax (query stx)
   (let ([lines (cdr (syntax->datum stx))])
-    (display lines)
-    (datum->syntax stx lines)))
+    ;(display lines)
+    (define lines-syntax-tree (parse-levels lines 0))
+    (display lines-syntax-tree)
+    (datum->syntax stx lines-syntax-tree)))
 
 (define-syntax-rule (magic-module-begin (magic QUERY ...))
   (#%module-begin QUERY ...))
