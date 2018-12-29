6ab0ce16e1b62783e4d85e88156e2b753b5d474e
jjsimpso <jjsimpso@gmail.com>
2018-12-29 19:20:59 +0000

    magic: add indirect offset support.
    fix string comparison to make null bytes explicit
    (needed for PE\0\0 for example)

--- file-exe.rkt
+++ file-exe.rkt
@@ -3,6 +3,8 @@
 # MS Windows executables are also valid MS-DOS executables
 0           string  MZ
 >0x18       leshort <0x40   MZ executable (MS-DOS)
+#>>(4.s*512) leshort 0x014c  COFF executable (MS-DOS, DJGPP)
+#>>(4.s*512) leshort !0x014c MZ executable (MS-DOS)
 # skip the whole block below if it is not an extended executable
 >0x18       leshort >0x3f
 >>(0x3c.l)  string  PE\0\0  PE executable (MS-Windows)
