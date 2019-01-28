33622d0e70ab49d93294ee2fe6c3d476ce135037
jjsimpso <jjsimpso@gmail.com>
2019-01-28 03:24:06 +0000

    magic: small steps toward getting the expander to work. cleaning up provided symbols. moved util functions to separate file so I can import for-syntax and test at runtime.
    may be best to move some magic-functions to macros in expander.rkt. also
    need to transform the lines-syntax-tree into sequence of when's. all
    branches at each level should execute except level 0.

--- expander.rkt
+++ expander.rkt
@@ -3,113 +3,27 @@
 (provide #%top #%top-interaction #%datum #%app)
 
 (require "magic-functions.rkt")
+(require (for-syntax "expander-utils.rkt"))
 
-(define-syntax (query1 stx)
+#;(define-syntax (query1 stx)
   (syntax-case stx ()
     [(query ...) 
      (let ([q (car (syntax->datum #'(query ...)))])
        (printf "q is ~a~n" q)
        (datum->syntax stx q))]))
 
-;; probably don't need these
-(define (make-expr test)
-  (list test))
+#;(define-syntax level
+  (syntax-rules ()
+    [(level line ...)
+     (cond [line #t]
+           ...)]
+    [_ expr]))
 
-(define (make-test func expr)
-  (cons func expr))
+(define-syntax-rule (level line ...)
+  (when line ...))
 
-(define (get-func test)
-  (car test))
-
-(define (get-expr test)
-  (cdr test))
-
-;; not final. doesn't work if level drops. need a new algorithm.
-;; probably not going to use
-(define (build-exp lines last-line cur-level new-level)
-  (cond 
-    [(null? lines) (make-expr (make-test (car last-line) '()))]
-    [(and (pair? (car lines)) 
-          (equal? (caar lines) 'level))
-     (printf "increment level~n") 
-     (build-exp (cdr lines) last-line cur-level (add1 new-level))]
-    [(= new-level (add1 cur-level))
-     (printf "add new line with higher level~n") 
-     (if (null? last-line)
-         (build-exp (cdr lines) 
-                    (car lines) 
-                    (add1 cur-level) 
-                    0)
-         (make-expr (make-test (car last-line) 
-                               (make-expr 
-                                (build-exp (cdr lines) 
-                                           (car lines) 
-                                           (add1 cur-level) 
-                                           0)))))]
-    [(= new-level cur-level)
-     (printf "new line at same level~n") 
-     (if (null? last-line)
-         (build-exp (cdr lines) 
-                    (car lines)
-                    cur-level 
-                    0)
-         (cons (make-test (car last-line) '()) 
-               (build-exp (cdr lines) (car lines) cur-level 0)))]
-    ;[(< new-level cur-level)
-     
-    ))
-
-(define-for-syntax (eat-lines-with-greater-level lines level)
-  (define next-line-level
-    (for/fold ([level 0])
-              ([line lines])
-      #:break (not (and (pair? line)
-                        (equal? (car line) 'level)))         
-          (add1 level)))
-
-  ;; drop any level lines from the lines list
-  (define next-lines (list-tail lines next-line-level))
-
-  ;(printf "nll = ~a, next lines = ~a~n" next-line-level next-lines)
-
-  (cond 
-    [(> next-line-level level)
-     (eat-lines-with-greater-level (cdr next-lines) level)]
-    [else
-     lines]))
-
-;; ex: (parse-levels '((line1) (level) (line2) (level) (level) (line3) (level) (line4) (level) (level) (line5)) 0)  
-(define-for-syntax (parse-levels lines level)
-  ;; count level of next line
-  (define next-line-level
-    (for/fold ([level 0])
-              ([line lines])
-      #:break (not (and (pair? line)
-                        (equal? (car line) 'level)))         
-          (add1 level)))
-  
-  ;; drop any level lines from the lines list
-  (define next-lines (list-tail lines next-line-level))
-  
-  (cond
-    [(null? lines) '()]
-    [(= next-line-level level)
-     (cons (car next-lines)
-           (parse-levels (cdr next-lines) level))]
-    [(= next-line-level (add1 level))
-     (cons (cons 'level 
-                 (cons (car next-lines) 
-                       (parse-levels (cdr next-lines) (add1 level))))
-           (parse-levels (eat-lines-with-greater-level lines level) level))]
-    [(< next-line-level level) '()]))
-
-;; (define-syntax level
-;;   (syntax-rules ()
-;;     [(level line ...)
-;;      (cond [line #t]
-;;            ...)]
-;;     [_ expr]))
-
+;; todo: switch to `syntax-parse` and use its `#:datum-literals` option or its `~datum` pattern form 
+;; to match raw datums without bindings
 (define-syntax line
   (syntax-rules (offset type test message)
     [(line (offset off) (type type-expr) (test test-expr)) 
@@ -118,20 +32,17 @@
      (magic-test (offset off) (type (quote type-expr) (quote test-expr)) (compare (quote test-expr)) msg)]
     [(_) "no clause found in line"]))
 
-(provide line)
-
 ;(define-syntax-rule (offset off)
 ;  off)
-(provide offset)
-(provide type)
-(provide compare)
 
 ;(define-syntax-rule (type expr)
-  
+
 (define-syntax (query stx)
+  ; investigate syntax->list so that i don't loose src location and other syntax info here
   (let ([lines (cdr (syntax->datum stx))])
     ;(display lines)
-    (define lines-syntax-tree (parse-levels lines 0))
+    ;(define lines-syntax-tree (parse-levels lines 0))
+    (define lines-syntax-tree (cons 'when (parse-levels lines 0)))
     (display lines-syntax-tree)
     (datum->syntax stx lines-syntax-tree)))
 
@@ -139,32 +50,6 @@
   (#%module-begin QUERY ...))
 
 (provide (rename-out [magic-module-begin #%module-begin])
-         query)
+         (all-from-out "magic-functions.rkt")
+         query line level when)
 
-
-
-
-;; sample walk of a tree of tests
-(define (line1) #t)
-(define (line2) #f)
-(define (line3) #t)
-(define (line4) #f)
-(define (line5) #f)
-
-(define test-exp `((,line1 ((,line2) (,line3 ((,line4) (,line5)))))))
-(define test-exp2 `((,line1) (,line2) (,line3)))
-
-(define (test-walk exp)
-  (cond [(null? exp) #f]
-        [(not (pair? exp)) #f]
-        [(list? (car exp)) 
-         (let ([result (test-walk (car exp))])
-           (if (eq? result 'match)
-               'match
-               (test-walk (cdr exp))))]
-        [((car exp))
-         (printf "matched ~a~n" (car exp))
-         (if (null? (cdr exp))
-             'match
-             (test-walk (cdr exp)))]
-        [else #f]))
--- parser-tester.rkt
+++ parser-tester.rkt
@@ -9,7 +9,7 @@
  (apply-tokenizer 
   make-tokenizer 
   ;"0	beshort	0x0206	ALAN game data\n>2	short	<10	version 2.6%d\n\n0	ubyte    >0	\n")
-  "0           string  MZ\n>0x18       leshort <0x40   MZ executable (MS-DOS)\n>0x18       leshort >0x3f\n>>(0x3c.l)  string  PE\\0\\0  PE executable (MS-Windows)\n")
+  "0           string  MZ\n>0x18       leshort <0x40   MZ executable (MS-DOS)\n>0x18       leshort >0x3f\n>>(0x3c.l)  string  PE\\0\\0  PE executable (MS-Windows)\n0	beshort	0x0206	ALAN game data\n")
   ;"(4.s*512)    leshort 0x014c  COFF executable (MS-DOS, DJGPP)\n")
   ;"(4.s*512) leshort !0x014c MZ executable (MS-DOS)\n")
 )
