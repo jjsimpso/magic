dd11e2dbc1dccea636901149f8c7599abd5e1160
jjsimpso <jjsimpso@gmail.com>
2019-04-19 18:46:34 +0000

    magic: improve lexer's string handling
      - add separate lexer to handle strings in the test field for string type 
        tests.
      - add a lexer to handle string flags in the type field. this currently
        only checks for the count flag.
      - update grammar's splices and cuts for search type(others to come later)

--- reader.rkt
+++ reader.rkt
@@ -14,21 +14,22 @@
   (strip-context #`(module magic-mod "expander.rkt"
                           #,parse-tree)))
 
+(define-lex-abbrev hws (:+ " " "\t"))
 (define-lex-abbrev digits (:+ (char-set "0123456789")))
 (define-lex-abbrev hex-digits (:+ (char-set "0123456789abcdefABCDEF")))
 (define-lex-abbrev op (:= 1 (char-set "<>=&^!+-*/%|")))
 (define-lex-abbrev paren (:= 1 (char-set "()")))
-(define-lex-abbrev string-chars (:+ (char-set "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") "\\"))
+;(define-lex-abbrev string-chars (complement (:+ " " "\t" "\n")))
+(define-lex-abbrev string-chars (:+ (char-set "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-.") "\\"))
 (define-lex-abbrev key-word (:or "byte" "short" "beshort" "leshort" "long" "belong" "lelong" "quad" "bequad" "lequad" "string" "search" "regex"))
 (define-lex-abbrev integer-type (:or "byte" "short" "beshort" "leshort" "long" "belong" "lelong" "quad" "bequad" "lequad"))
+(define-lex-abbrev string-type (:or "string" "search" "regex"))
 (define-lex-abbrev size-specifier (:= 1 (char-set "bBcCshSHlLm")))
 
 (define (magic-string-to-racket s)
   ;(string-replace s "\\0" "\u0000"))
   ; convert raw string bytes to racket unicode representation
   (regexp-replace* #px"\\\\(\\d{1,2})" s "\u00\\2"))
-
-(define hws-count 0)
 
 (define magic-lexer
   (lexer-srcloc
@@ -46,7 +47,7 @@
       (displayln "newline found1") 
       (set! hws-count 0)
       (token 'EOL))]
-   [(:+ " " "\t") 
+   [hws 
     (begin 
       (set! hws-count (add1 hws-count))
       (token 'HWS #:skip? #f))]
@@ -54,6 +55,10 @@
    [op (token lexeme lexeme)]
    [paren (token lexeme lexeme)]
    [(:seq "." size-specifier) (token lexeme lexeme)]
+   [string-type 
+    (begin
+      (set! current-lexer magic-lexer-string-flags)
+      (token lexeme lexeme))]
    [key-word (token lexeme lexeme)]
    [(:seq "u" integer-type) (let ([pos (file-position input-port)])
                               (file-position input-port (- pos 
@@ -63,11 +68,11 @@
    [digits (token 'INTEGER (string->number lexeme))]
    [(:seq "-" digits) (token 'INTEGER (string->number lexeme))]
    [(:seq "0x" hex-digits) (token 'INTEGER (string->number (substring lexeme 2) 16))]
-   [string-chars (token 'STRING (magic-string-to-racket lexeme))]
+   ;[string-chars (token 'STRING (magic-string-to-racket lexeme))]
    ;[any-char (token 'CHAR lexeme)]))
 ))
 
-(define magic-lexer2
+(define magic-lexer-message
   (lexer-srcloc
    ["\n" 
     (begin
@@ -77,12 +82,34 @@
    [(from/stop-before any-char "\n") 
     (begin
       (set! hws-count 0)
-      (token 'STRING lexeme))]))
+      (token 'STRING (magic-string-to-racket lexeme)))]))
    ;[any-string (token 'STRING lexeme)]))
+
+(define magic-lexer-string-flags
+  (lexer-srcloc
+   ["/" (token lexeme lexeme)]
+   [digits (token 'INTEGER (string->number lexeme))]
+   [hws
+    (begin 
+      (set! hws-count (add1 hws-count))
+      (set! current-lexer magic-lexer-string)
+      (token 'HWS #:skip? #f))]))
 
 (define magic-lexer-string
   (lexer-srcloc
-   [string-chars (token 'STRING lexeme)]))
+   [hws
+    (begin 
+      (set! hws-count (add1 hws-count))
+      (token 'HWS #:skip? #f))]
+   ;[string-chars
+   [(from/stop-before (:~ " " "\t")  (:or " " "\t" "\n"))
+    (begin
+      (set! current-lexer magic-lexer)
+      (token 'STRING (magic-string-to-racket lexeme)))]))
+
+
+(define hws-count 0)
+(define current-lexer magic-lexer)
 
 (define (make-tokenizer port [path #f])
   (port-count-lines! port)
@@ -91,9 +118,9 @@
     (cond [(= hws-count 3)
            (begin
              (displayln "calling lexer2")
-             (magic-lexer2 port))]
+             (magic-lexer-message port))]
           [else 
-           (magic-lexer port)]))
+           (current-lexer port)]))
   next-token)
 
 ;; test function
