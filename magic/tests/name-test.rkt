c85977ab326b1e497be7a753f3f0e72caffe0127
jjsimpso <jjsimpso@a8c7db1f-5b64-4c47-b761-cd94ac1c0e2a>
2019-07-13 17:35:03 +0000

    magic: add test for name scope bug

--- magic/tests/name-test.rkt
+++ magic/tests/name-test.rkt
@@ -3,3 +3,9 @@
 #	display tga bitmap image information
 0	name				tga-image
 >2	ubyte		<34		Targa image data
+
+
+0	name				test-scope
+>2	ubyte		<34		Test Scope
+>0      use             tga-image
+
