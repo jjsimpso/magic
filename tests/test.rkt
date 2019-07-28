80e43a2680a3f11314098e3ce1a502fd601d8b05
jjsimpso <jjsimpso@gmail.com>
2019-07-28 23:13:50 +0000

    magic: add Apache 2.0 License header. remove unused code.

--- tests/test.rkt
+++ tests/test.rkt
@@ -1,5 +1,19 @@
 #lang racket
 
+;;Copyright 2019 Jonathan Simpson
+;;
+;;   Licensed under the Apache License, Version 2.0 (the "License");
+;;   you may not use this file except in compliance with the License.
+;;   You may obtain a copy of the License at
+;;
+;;       http://www.apache.org/licenses/LICENSE-2.0
+;;
+;;   Unless required by applicable law or agreed to in writing, software
+;;   distributed under the License is distributed on an "AS IS" BASIS,
+;;   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;;   See the License for the specific language governing permissions and
+;;   limitations under the License.
+
 (require (only-in "file-exe.rkt" (magic-query exe-query) (magic-query-run-all exe-query-all)))
 (require (only-in "debugtest.rkt" (magic-query image-query)))
 (require (only-in "string-test.rkt" (magic-query string-query)))
