5bae06caac1ffc2091009d30a517d1a4efdc3478
Jonathan Simpson <jjsimpso@gmail.com>
2021-10-21 21:17:44 -0400

    Cut EOL from starting magic rule.

--- parser.rkt
+++ parser.rkt
@@ -14,7 +14,7 @@
 ;;   See the License for the specific language governing permissions and
 ;;   limitations under the License.
 
-magic : (query | named-query | EOL)+
+magic : (query | named-query | /EOL)+
 query : line (level+ (line | clear-line))*
 level : /">"
 line : offset /HWS type /HWS test (/HWS message?)? /EOL
